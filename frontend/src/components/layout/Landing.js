970393da90a9dbf781721af48885987c658de5b1
“Nicholas <n.samaras4@outlook.com>
2023-05-24 16:39:47 -0400

    update

--- frontend/src/components/layout/Landing.js
+++ frontend/src/components/layout/Landing.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios';
+
 
 
 const Landing = props => {
@@ -18,25 +20,46 @@ const Landing = props => {
       window.removeEventListener('resize', handleResize);
     };
   }, []);
+
+  const handleDownload = () => {
+    axios({
+      url: '/api/book', // Replace with your backend endpoint for downloading the book
+      method: 'GET',
+      responseType: 'blob', // Set the response type to 'blob' for binary data
+    })
+      .then(response => {
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', 'mevlana.pdf'); // Specify the filename for the downloaded file
+        document.body.appendChild(link);
+        link.click();
+      })
+      .catch(error => {
+        console.error('Error downloading the book:', error);
+      });
+  };
   return (
     <div style={{ paddingTop: isSmallScreen ? '160px' : '0' }}>
     <section className="landing" style={{ paddingTop: isSmallScreen ? '160px' : '0' }}>
         <div className="dark-overlay">
-          <div className="landing-inner">
-            <h1 className="x-large">Random Layout</h1>
-            <p className="lead">
+          <div className="landing-inner">     <p className="lead">
               Connect with like minds and share the experience
             </p>
             <div className="buttons">
-              <Link to="/register" className="btn btn-primary landing-btn">
+              <Link to="/register" className="btn btn-dark landing-btn ">
                 Sign Up
               </Link>
-              <Link to="/login" className="btn btn-light landing-btn">
+              <Link to='/' className="btn btn-dark landing-btn" onClick={handleDownload}>
+                Download the book
+              </Link>
+              <Link to="/login" className="btn btn-dark landing-btn">
                 Login
               </Link>
-              <Link to="/contact" className="btn btn-primary landing-btn">
+              <Link to="/contact" className="btn btn-dark landing-btn">
                 Contact Form
               </Link>
+              
             </div>
           </div>
         </div>
